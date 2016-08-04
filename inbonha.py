c7a97864fd6089a136da5dcb676243a84d972e2c
Thales Francisco <thales@alvesnet.net>
2016-08-03 23:16:35 -0300

    é issaê

---
+++ inbonha.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from random import randint
+
+maximo_silabas = 4
+silabas = ['MÔ','NI','CA','CE','BO','LI','NHA','MA','GA','LI','CAS','CÃO','BI','DU','IN']
+flags = [True] * len(silabas)
+
+def numero():
+	return randint(2,maximo_silabas)
+
+def nome():
+	inbonha = ''
+	for i in range(numero()):
+		x = randint(0,len(silabas))
+		if flags[x] == True:
+			inbonha += silabas[x]
+			flags[x] = False
+	
+	return inbonha
+	
+print(nome())
+	
